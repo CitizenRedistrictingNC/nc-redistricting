2a3405e654cd841413c350c126f29925b8161898
Dane Summers <dsummersl@yahoo.com>
2016-07-25 22:27:15 -0400

    Adds score page; improves rankings mockups.

--- app/personal.rankings.component.ts
+++ app/personal.rankings.component.ts
@@ -4,13 +4,38 @@ import { Component } from '@angular/core';
   selector: 'my-app',
 	template: `
     <div class="row starter-row">
-      <ul class="unstyled">
-        <li> <button class="btn btn-warning">TODO</button> Add table</li>
-      </ul>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th class="text-right">Date</th>
+            <th class="text-right">Score</th>
+            <th>Map</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>First Map</td>
+            <td class="text-right">3/16/2016</td>
+            <td class="text-right">6.2</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>Second Map</td>
+            <td class="text-right">3/25/2016</td>
+            <td class="text-right">6.9</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>Third map</td>
+            <td class="text-right">5/5/2016</td>
+            <td class="text-right">8.2</td>
+            <td>...</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   `
 })
 
-export class PersonalRankingsComponent {
-  title = 'Personal Rankings Component';
-}
+export class PersonalRankingsComponent { }
--- app/rankings.component.ts
+++ app/rankings.component.ts
@@ -4,9 +4,32 @@ import { Component } from '@angular/core';
   selector: 'my-app',
 	template: `
     <div class="row starter-row">
-      <ul class="unstyled">
-        <li> <button class="btn btn-warning">TODO</button> Add table</li>
-      </ul>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th class="text-right">Score</th>
+            <th>Map</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Best Score</td>
+            <td class="text-right">9.5</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>Second Best</td>
+            <td class="text-right">9.0</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>Third Best Score</td>
+            <td class="text-right">6.5</td>
+            <td>...</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   `
 })
