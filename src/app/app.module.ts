1248db8ea39c378ed409dc069cfaad7892ac19c9
Dane Summers <dsummersl@yahoo.com>
2016-08-22 11:57:50 -0400

    Hooks angular configuration into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { AppComponent  } from './app.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAdd3xy0z4sPwgiTUIYfoJ5KzpcHVWAjIE",
+  authDomain: "nc-citizenredistrict.firebaseapp.com",
+  databaseURL: "https://nc-citizenredistrict.firebaseio.com",
+  storageBucket: "nc-citizenredistrict.appspot.com",
+};
 
 @NgModule({
   declarations: [ AppComponent ],
-  imports: [ BrowserModule, RouterModule, RouterModule.forRoot(routes) ],
+  imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
